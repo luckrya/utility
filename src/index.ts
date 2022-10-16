2473c580325a36677d35b65fa692e6c01b290660
L.C <hnx.lemon@gmail.com>
2022-10-16 18:12:16 +0800

    fix: change export rule

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import * as DataTypeJudgment from "./data-type-judgment";
-import * as Environment from "./environment";
-
-export default {
-  ...DataTypeJudgment,
-  ...Environment,
-};
-
 export * from "./data-type-judgment";
 export * from "./environment";
